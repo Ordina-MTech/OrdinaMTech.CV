c99aefbcc682ebc7313b72e8220a7f1209ee9a8b
johnbruin <john.bruin@telfort.nl>
2023-05-22 23:09:00 +0200

    Fix

--- OrdinaMTech.CV.Data/CvContext.cs
+++ OrdinaMTech.CV.Data/CvContext.cs
@@ -8,12 +8,6 @@ namespace OrdinaMTech.Cv.Data
     {
         public DbSet<Models.Cv> Cvs { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseCosmos(
-            "https://cosmos-cv.documents.azure.com:443/",
-            "nqcNvgDGEq7CIwBo7gr6dD1mYt4uzIJnK3lsv4sJKp8cLFpV9xgLKVYPksFFqLFcaA7vryzhUNi6ACDbR2ZnrQ==",
-            databaseName: "Cv");
-
         public CvContext(DbContextOptions<CvContext> options) : base(options)
         {
         }
